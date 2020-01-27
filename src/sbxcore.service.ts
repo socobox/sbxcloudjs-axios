372a06880b8dfba3d47e880b70353b3c128dcfd7
Alexander Taborda Acosta <alextab93@gmail.com>
2020-01-27 10:50:39 -0500

    minor fix

--- src/sbxcore.service.ts
+++ src/sbxcore.service.ts
@@ -259,7 +259,7 @@ export class SbxCoreService extends SbxCore {
    * @param params
    * @param {boolean} [test=false]
    */
-  run(key: string, params: any, test?: boolean = false) {
+  run(key: string, params: any, test: boolean = false) {
     return this.httpClient.post(this.$p(this.urls.cloudscript_run), {key: key, params: params, test}).then((data: any) => {
       if (!data.success) {
         return {success: false, error: data.message || data.error || "There was an error. Please try again later."};
